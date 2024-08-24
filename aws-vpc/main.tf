8a99ff313367b69eea85b4b6a039059b4da3014d
Felipe Carvalho Funck <funck.felipe@hotmail.com>
2024-08-24 14:15:13 -0300

    Creating aws networking

---
+++ aws-vpc/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= v1.9.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.63.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "fcfunck-remote-state"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "felipe.funck"
+      managed-by = "terraform"
+    }
+  }
+}
